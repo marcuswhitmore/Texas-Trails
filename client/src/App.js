7b0058a93a444919f4136b8be502d4b9483da426
Thomas Q Raya <thomas_raya@hotmail.com>
2019-06-22 13:45:35 -0500

    uncommented lines 2 & 5 in client/src/app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-// import Form from "./components/Form";
+import Form from "./components/Form";
 import LoginBox from "./components/Login";
 import SignUp from "./components/SignUp";
-// import SVG from "./components/SVG/SVG";
+import SVG from "./components/SVG/SVG";
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
