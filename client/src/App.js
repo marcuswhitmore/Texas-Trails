fe53a52f53bc1774c89c30d5505e6bece16ae168
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2019-06-22 13:11:51 -0500

    new commits

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,20 @@
 import React from "react";
 // import Form from "./components/Form";
-//  import LoginBox from "./components/Login";
-// import SignUp from "./components/SignUp";
-  import SVG from "./components/SVG/SVG";
+import LoginBox from "./components/Login";
+import SignUp from "./components/SignUp";
+// import SVG from "./components/SVG/SVG";
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
-
   return (
     <Router>
       <div>
-
         <Route exact path="/" component={LoginBox} />
         <Route path="/SignUp" component={SignUp} />
-        
       </div>
     </Router>
   );
 }
 
-
-
 export default App;
-
