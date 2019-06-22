16afc450f6e6a182c3f1f203a6810f313818ada8
Marcus Whitmore <marcus.whitmore@gmail.com>
2019-06-22 13:09:34 -0500

    json object

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-// import Form from "./components/Form";
-//  import LoginBox from "./components/Login";
-// import SignUp from "./components/SignUp";
+import Form from "./components/Form";
+ import LoginBox from "./components/Login";
+import SignUp from "./components/SignUp";
   import SVG from "./components/SVG/SVG";
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
