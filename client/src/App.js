8db8a341e8b677d0162080fa3e380cabf959ac6d
Marcus Whitmore <marcus.whitmore@gmail.com>
2019-06-22 11:09:40 -0500

    svg changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import Form from "./components/Form";
 import LoginBox from "./components/Login";
 import SignUp from "./components/SignUp";
-import SVG from "./components/SVG"
+import SVG from "./components/SVG";
 
 function App() {
-  return <SignUp />;
-  return <SVG />;
+  return <SVG/>
+
 }
 
 export default App;
--- client/src/components/SVG/SVG.js
+++ client/src/components/SVG/SVG.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
-import './SVG.css';
-
-function SVG() {
-  return (
-    <div className="SVG">
-      <header className="SVG-header">
-        <img src={logo} className="SVG-logo" alt="logo" />
-        <p>
-          Edit <code>src/SVG.js</code> and save to reload.
-        </p>
-        <a
-          className="SVG-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './style.css';
+class SVG extends React.Component {
+  render() {
+    return (
+      <div className="SVG">
+        <header className="SVG-header">
+          <img src={logo} className="SVG-logo" alt="logo" />
+          <p>
+            Edit <code>src/SVG.js</code> and save to reload.
+          </p>
+          <a
+            className="SVG-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
+
 
 export default SVG;
