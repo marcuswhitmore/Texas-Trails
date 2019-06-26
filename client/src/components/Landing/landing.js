1e9f75427dff2ee625a5384fb0c07f045f5c2fda
johnbroaddusivccv <johnbroaddusivccv@gmail.com>
2019-06-25 19:52:10 -0500

    Layne and I sat here to figure out what was going on with our app in class Tues

---
+++ client/src/components/Landing/landing.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Landing extends Component {
+  render() {
+    return <h1>This is Going to be the next page to our application</h1>;
+  }
+}
+
+export default Landing;
